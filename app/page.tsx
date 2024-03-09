c6d70f2b8171c02ebade97ed0d12cd784b086078
Arun <aj7433413@gmail.com>
2024-03-09 09:24:18 +0530

    created header component

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Header from "@/components/header";
+export default function Home() {
+  return (
+    <main className="h-full w-full">
+      <Header />
+    </main>
+  );
+}
